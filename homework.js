163ac576af18ead0aca4c8062f9b754dd4e6cd8a
AnyaTyu <anyatyu@bk.ru>
2014-01-27 20:05:52 +0200

    first commit

--- homework.js
+++ homework.js
@@ -1,22 +1,35 @@
-/**
- * Обработчик клика по ссылке с классом 'popup-link'
- * @param {Event} e событие клика
- * @private
- */
-function _onMouseClick(e) {}
+	window.onload =  function () {
+	  var links =  document.getElementsByClassName('popup-link');
+	  for (var i=0, max = links.length; i < max; i ++)
+	  	links[i].onclick = function(){
+	  		openPopupFromLink(_onMouseClick(this));
+	  		return false;
+	  	};
+}
+function _onMouseClick(e) {
+ var links = new Object();
+ links._url = e.attributes[0].nodeValue;
+ links.message = e.attributes[3].nodeValue;
+ links.title = e.attributes[2].nodeValue;
+ //alert(links);
+ return links;
+}
 
-/**
- * Получает данные из ссылки
- * на основе этих данных создаёт попап (через createPopup) и добавляет его в DOM
- * @param {HTMLElement} link Ссылка с data-аттрибутами
- */
-function openPopupFromLink(link) {}
+function openPopupFromLink(links) {
+	var _url = links._url;
+	var message = links.message;
+	var title = links.title;
+	message = message.replace("\'%s\'",_url);
+var myPopUp = document.createElement('div');
+myPopUp.id = "popUp";
+myPopUp.innerHTML = "<div id=\"tt\"><p>\""+title+"\"</p><p>\""+message+"\"</p><form><input type = \"button\" value = \"Да\" id = \"yes\" onclick = \"go('"+_url+"');\"/><input type = \"button\" value = \"Нет\" id = \"no\" onclick=\"del()\"/></form></div>"
+document.body.appendChild(myPopUp);}
 
-/**
- * Создаёт DOM-узел с сообщением
- * @param {String} title Заголовок сообщение
- * @param {String} message Текст сообщения сообщение
- * @param {Function} onOk Обработчик клика по кнопке 'Да'
- * @returns {HTMLElement}
- */
-function createPopup(title, message, onOk) {}
+function go(_url){
+window.location = _url;
+}
+
+function del(){
+	 var b = document.getElementById('popUp')
+	 var c = document.body.removeChild(b);
+}
