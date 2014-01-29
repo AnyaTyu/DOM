a25c7014f06ae19b65f4f9f083cc647ac0977f36
AnyaTyu <annatyu@bk.ru>
2014-01-29 12:36:49 +0200

    Second commit

--- homework.js
+++ homework.js
@@ -1,35 +1,52 @@
-	window.onload =  function () {
-	  var links =  document.getElementsByClassName('popup-link');
-	  for (var i=0, max = links.length; i < max; i ++)
-	  	links[i].onclick = function(){
-	  		openPopupFromLink(_onMouseClick(this));
-	  		return false;
-	  	};
+/**
+ * Обработчик клика по ссылке с классом 'popup-link'
+ * @param {Event} e событие клика
+ * @private
+ */
+document.body.onclick = function(e){
+	if (e.target.className === "popup-link") _onMouseClick(e);
 }
 function _onMouseClick(e) {
- var links = new Object();
- links._url = e.attributes[0].nodeValue;
- links.message = e.attributes[3].nodeValue;
- links.title = e.attributes[2].nodeValue;
- //alert(links);
- return links;
+	if (e.preventDefault) {
+		e.preventDefault();
+	}
+	openPopupFromLink(e.target);
 }
 
-function openPopupFromLink(links) {
-	var _url = links._url;
-	var message = links.message;
-	var title = links.title;
+/**
+ * Получает данные из ссылки
+ * на основе этих данных создаёт попап (через createPopup) и добавляет его в DOM
+ * @param {HTMLElement} link Ссылка с data-аттрибутами
+ */
+function openPopupFromLink(link) {
+	var _url = link.getAttribute('href');
+	var message = link.getAttribute('data-message');
 	message = message.replace("\'%s\'",_url);
-var myPopUp = document.createElement('div');
-myPopUp.id = "popUp";
-myPopUp.innerHTML = "<div id=\"tt\"><p>\""+title+"\"</p><p>\""+message+"\"</p><form><input type = \"button\" value = \"Да\" id = \"yes\" onclick = \"go('"+_url+"');\"/><input type = \"button\" value = \"Нет\" id = \"no\" onclick=\"del()\"/></form></div>"
-document.body.appendChild(myPopUp);}
+	var title = link.getAttribute('data-title');
+	var myPopUp = createPopup(title,message,_url);
+	document.body.appendChild(myPopUp);
+}
+
+/**
+ * Создаёт DOM-узел с сообщением
+ * @param {String} title Заголовок сообщение
+ * @param {String} message Текст сообщения сообщение
+ * @param {Function} onOk Обработчик клика по кнопке 'Да'
+ * @returns {HTMLElement}
+ */
+function createPopup(title, message, _url) {
+	var myPopUp = document.createElement('div');
+	myPopUp.id = "popUp";
+	myPopUp.innerHTML = "<div id=\"tt\"><p>\""+title+"\"</p><p>\""+message+"\"</p><form><input type = \"button\" value = \"Да\" id = \"yes\" onclick = \"go('"+_url+"');\"/><input type = \"button\" value = \"Нет\" id = \"no\" onclick=\"del()\"/></form></div>"
+	//document.body.appendChild(myPopUp);
+	return myPopUp;
+}
 
 function go(_url){
-window.location = _url;
+	window.location = _url;
 }
 
 function del(){
-	 var b = document.getElementById('popUp')
-	 var c = document.body.removeChild(b);
+	 var del = document.getElementById('popUp')
+	 document.body.removeChild(del);
 }
