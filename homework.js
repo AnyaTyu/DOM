804679e3344d6de9d666242303850cafffd0d4e9
AnyaTyu <anyatyu@bk.ru>
2014-02-03 16:19:36 +0200

    7 com

--- homework.js
+++ homework.js
@@ -1,10 +1,9 @@
+document.body.addEventListener('click', _onMouseClick);
 /**
  * Обработчик клика по ссылке с классом 'popup-link'
  * @param {Event} e событие клика
  * @private
  */
-document.body.addEventListener('click', _onMouseClick);
-
 function _onMouseClick(e) {
 	if (e.preventDefault) {
 		e.preventDefault();
